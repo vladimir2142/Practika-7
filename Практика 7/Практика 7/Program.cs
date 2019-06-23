b96129bc7000226186af5ee220c05b191cc77f42
vladimir2142 <49162341+vladimir2142@users.noreply.github.com>
2019-06-23 14:55:38 +0500

    Add files via upload

---
+++ Практика 7/Практика 7/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практика_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n, k;
+            String s;
+            Console.WriteLine("Введите n");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите k");
+            k = int.Parse(Console.ReadLine());
+            s = "";
+            string s1 = "";
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    
+                    
+                        s = i.ToString() + j.ToString();
+                        s1 = s1 + s + ", ";
+                    
+                   
+                }
+                s = "";
+
+            }
+            Console.WriteLine(s1);
+           
+            Console.Read();
+        }
+    }
+}
